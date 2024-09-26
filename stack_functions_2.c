12152c13e2c8c7471faa55a634ec6ca32f3c3ee3
doehab <dohaehab@gmail.com>
2024-09-26 16:25:28 -0700

    fix add error msg

--- stack_functions_2.c
+++ stack_functions_2.c
@@ -18,13 +18,11 @@ void add(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	first_elem = (*stack)->n;
+	(*stack)->next->n += (*stack)->n;
 	pop(stack, line_number);
-	second_elem = first_elem + (*stack)->n;
-	(*stack)->n = second_elem;
 }
 
 /**
