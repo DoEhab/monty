ea4af5fd6648c06aa49dfb174602780747aeedd5
doehab <dohaehab@gmail.com>
2024-09-26 16:08:57 -0700

    updated files

--- stack_functions_1.c
+++ stack_functions_1.c
@@ -94,7 +94,7 @@ void pint(stack_t **stack, unsigned int line_number)
 void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp = NULL;
-	
+
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
@@ -127,43 +127,6 @@ void swap(stack_t **stack, unsigned int line_number)
 	temp = *stack;
 	*stack = (*stack)->next;
 	(*stack)->prev = NULL;
-	(*stack)->next = temp;	
+	(*stack)->next = temp;
 }
 
-/**
-  * add - add top two element from stack
-  *
-  * @stack: stack pointer
-  * @line_number: the command line number
-  * Return: void
-  */
-
-void add(stack_t **stack, unsigned int line_number)
-{
-	int first_elem;
-	int second_elem;
-
-	if (*stack == NULL || (*stack)->next == NULL)
-	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
- 		exit(EXIT_FAILURE);
-	}
-	first_elem = (*stack)->n;
-	pop(stack, line_number);
-	second_elem = first_elem + (*stack)->n;
-	(*stack)->n = second_elem;
-}
-
-/**
-  * nop - remove top element from stack
-  *
-  * @stack: stack pointer
-  * @line_number: the command line number
-  * Return: void
-  */
-
-void nop(stack_t **stack, unsigned int line_number)
-{
-	(void) line_number;
-	(void) stack;
-}
